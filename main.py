02fd104859c8591c937251141dd6b2860e1e98c4
Charles Smith <binarskugga@gmail.com>
2022-10-24 00:01:49 -0400

    build frontend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 
 fastapi = FastAPI()
+fastapi.mount("/", StaticFiles(directory="frontend/dist", html=True), name="frontend")
 
 
 @fastapi.get('/test')
