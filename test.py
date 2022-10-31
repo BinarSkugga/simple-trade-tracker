3d20e7e24c480a46e4188073e6fff71ef047fccf
Charles Smith <binarskugga@gmail.com>
2022-10-31 02:40:35 -0400

    oops

--- test.py
+++
@@ -1,7 +0,0 @@
-import pyotp
-
-totp = pyotp.totp.TOTP('JDRSMBJVAXDLHKLD').provisioning_uri(name='Wealthsimple:binarskugga@gmail.com', issuer_name='Wealthsimple')
-totp = pyotp.parse_uri(totp)
-print(totp.now())
-
-
