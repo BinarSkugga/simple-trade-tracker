be3dd698e0eff713669ea8b0e8c3ec7eb4e89809
Charles Smith <charles@sollum.tech>
2022-12-27 19:02:03 -0500

    added a postgres database to the actions and fixed up import during testing phase

---
+++ backend/tests/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+
+from database_utils import execute, drop_database, create_database
+from models import user, ws_stock, ws_position
+from repository import Repository
+
+
+@pytest.fixture(autouse=True)
+def initialize_schema():
+    drop_database('trade_tracker_test')
+    create_database('trade_tracker_test')
+
+    execute(user.SQL_SCHEMA, fetch=False)
+    execute(ws_stock.SQL_SCHEMA, fetch=False)
+    execute(ws_position.SQL_SCHEMA, fetch=False)
+
+
+@pytest.fixture
+def user_repository():
+    return Repository('user', user.User)
+
+
+@pytest.fixture
+def stock_repository():
+    return Repository('stock', ws_stock.WSStock)
+
+
+@pytest.fixture
+def position_repository():
+    return Repository('position', ws_position.WSPosition)
